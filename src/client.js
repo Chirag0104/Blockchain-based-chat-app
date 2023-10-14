45b39cfaa7b11c979b8b8d570ef0163051969e70
Chirag Goyal <73928620+Chirag0104@users.noreply.github.com>
2023-10-14 14:58:10 +0530

    Add files via upload

---
+++ src/client.js
@@ -0,0 +1,9 @@
+import sanityClient from "@sanity/client";
+export default sanityClient({
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID, // find this at manage.sanity.io or in your sanity.json
+  dataset: "production", // this is from those question during 'sanity init'
+  token: import.meta.env.VITE_SANITY_TOKEN,
+  useCdn: true,
+  apiVersion: '2021-08-31',
+  ignoreBrowserTokenWarning: true,
+});
